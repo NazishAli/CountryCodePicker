83cad96f3f7df133f2f52f5e3969d6845d3585a2
Nazish Ali <nazish.ali@quickli.com>
2016-10-05 13:24:46 +0530

    add viewController

---
+++ Example/CountryCodePicker/CountryViewController.m
@@ -0,0 +1,37 @@
+//
+//  CountryViewController.m
+//  CountryCodePicker
+//
+//  Created by Quickli on 05/10/16.
+//  Copyright © 2016 Nazish Ali. All rights reserved.
+//
+
+#import "CountryViewController.h"
+
+@interface CountryViewController ()
+
+@end
+
+@implementation CountryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
